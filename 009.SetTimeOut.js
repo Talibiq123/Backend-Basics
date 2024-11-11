fc6f4a8a36842ec4ca0592c292a1afc06a0397ec
Talibiq123 <talibsaeed90@gmail.com>
2024-11-11 21:17:09 +0530

    node...

---
+++ 009.SetTimeOut.js
@@ -0,0 +1,7 @@
+// starting operating system process
+console.log('First');
+setTimeout(() => {
+    console.log('Second');
+}, 0)
+console.log('Third');
+// completed and existed operatiohng system process
