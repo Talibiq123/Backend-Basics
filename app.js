de6add5ab18308698e80789691d9f03846445431
Talibiq123 <talibsaeed90@gmail.com>
2024-11-19 19:17:38 +0530

    middleware...

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const path = require('path');
 
 const app = express();
 
+// Set up status and middleware
 // app.use(express.static('./public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
 
 app.all('*', (req, res) => {
   res.status(404).send('<h1>Page Not Found</h1>');
